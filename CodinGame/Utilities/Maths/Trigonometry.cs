3bf357394244a96ccf57ebb0597db4ea571a3850
Aaron Passchier <ap007@aquinas.edu>
2020-12-03 10:38:15 -0500

    Added getting x and y values from distance ang angle

--- CodinGame/Utilities/Maths/Trigonometry.cs
+++ CodinGame/Utilities/Maths/Trigonometry.cs
@@ -4,14 +4,14 @@ namespace CodinGame.Utilities.Maths
 {
     public static class Trigonometry
     {
-        public static double GetDistance(int x1, int y1, int x2, int y2)
+        public static double GetDistance(double x1, double y1, double x2, double y2)
         {
             var xDistance = x1 - x2;
             var yDistance = y1 - y2;
             return Math.Sqrt(xDistance * xDistance + yDistance * yDistance);
         }
 
-        public static double GetAngle(int x1, int y1, int x2, int y2)
+        public static double GetAngle(double x1, double y1, double x2, double y2)
         {
             var xDistance = x1 - x2;
             var yDistance = y1 - y2;
@@ -21,12 +21,22 @@ namespace CodinGame.Utilities.Maths
             return angle;
         }
 
-        public static double GetHorizontalSpeedFraction(int angle, int decimals = 3)
+        public static double GetY(double angle, double xLength, int decimals = 3)
+        {
+            return Math.Round(Math.Tan(ToRadians(angle)) * xLength, decimals);
+        }
+
+        public static double GetX(double angle, double yLength, int decimals = 3)
+        {
+            return Math.Round(yLength / Math.Tan(ToRadians(angle)), decimals);
+        }
+
+        public static double GetHorizontalSpeedFraction(double angle, int decimals = 3)
         {
             return Math.Round(Math.Cos(ToRadians(angle)), decimals);
         }
 
-        public static double GetVerticalSpeedFraction(int angle, int decimals = 3)
+        public static double GetVerticalSpeedFraction(double angle, int decimals = 3)
         {
             return Math.Round(Math.Sin(ToRadians(angle)), decimals);
         }
