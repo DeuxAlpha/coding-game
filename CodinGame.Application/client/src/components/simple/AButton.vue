f5555b9491753c56c8152c1960a8b5ebf1930cd2
Aaron Passchier <ap007@aquinas.edu>
2020-12-05 14:41:44 -0500

    Added crappy AButton component

---
+++ CodinGame.Application/client/src/components/simple/AButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="mx-1 bg-blue-400 p-2 text-white">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent, computed, SetupContext, ref, reactive, watch} from 'vue';
+
+export default defineComponent({
+  name: 'AButton',
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
